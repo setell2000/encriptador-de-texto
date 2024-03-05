caacd07997e277cba44366fb849889ed0545de6f
Gabriel <gabrielpp1987@gmail.com>
2024-03-05 17:31:53 -0600

    ajuste de espacios

--- main.js
+++ main.js
@@ -8,48 +8,41 @@ var desencriptar;
 
 botonEncriptar.addEventListener('click', function(){
     
-encriptar=document.getElementById('areaIngresar').value;
-encriptarMin=encriptar.toLowerCase();
+    encriptar=document.getElementById('areaIngresar').value;
+    encriptarMin=encriptar.toLowerCase();
 
-encriptarMin=encriptarMin
-.replaceAll("e","enter")
-.replaceAll("i","imes")
-.replaceAll("a","ai")
-.replaceAll("o","ober")
-.replaceAll("u","ufat");
+    encriptarMin=encriptarMin
+    .replaceAll("e","enter")
+    .replaceAll("i","imes")
+    .replaceAll("a","ai")
+    .replaceAll("o","ober")
+    .replaceAll("u","ufat");
 
-marginArea.value=encriptarMin;
+    marginArea.value=encriptarMin;
 
 
 });
 botonDesencriptar.addEventListener('click', function(){
     
-desencriptar=document.getElementById('areaIngresar').value;
+    desencriptar=document.getElementById('areaIngresar').value;
 
-desencriptar=desencriptar
-.replaceAll("enter","e")
-.replaceAll("imes","i")
-.replaceAll("ai","a")
-.replaceAll("ober","o")
-.replaceAll("ufat","u")
+    desencriptar=desencriptar
+    .replaceAll("enter","e")
+    .replaceAll("imes","i")
+    .replaceAll("ai","a")
+    .replaceAll("ober","o")
+    .replaceAll("ufat","u")
 
-marginArea.value=desencriptar;
-
-
-
+    marginArea.value=desencriptar;
 })
 
 copiar.addEventListener('click', function(){
     
-    try {
-        navigator.clipboard.writeText(document.getElementById('marginArea').value);
-    } catch (error) {
-        console.log('error al copiar');
-    }
-
-
-
-
+        try {
+            navigator.clipboard.writeText(document.getElementById('marginArea').value);
+        } catch (error) {
+            console.log('error al copiar');
+        }
 });
 
 
